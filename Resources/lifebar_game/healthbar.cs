bbe8413cd1b91b69479f7fb8092484cae1864909
Frenzy_R <rubenverrod@gmail.com>
2024-01-24 16:32:43 +0100

    healthbar almost implemented

--- Resources/lifebar_game/healthbar.cs
+++ Resources/lifebar_game/healthbar.cs
@@ -7,16 +7,32 @@ public partial class healthbar : ProgressBar
 {
 	public Timer timer = null;
 	public ProgressBar damageBar = null;
+	private float _health;
 	public float health 
 	{ 
 		get
 			{
-			 	return health;	
+			 	return _health;	
 			} 
 		set
 			{
-				float previous_health = health;
+				float previous_health = _health;
+				_health = (float)Math.Min(this.MaxValue, health);
+				this.Value = _health;
 				
+				if(_health <= 0)
+				{
+					QueueFree();
+				}
+				
+				if(_health < previous_health)
+				{
+					this.timer.Start();
+				}
+				else
+				{
+					this.damageBar.Value = _health;
+				}
 			} 
 	}
 	
@@ -40,4 +56,13 @@ public partial class healthbar : ProgressBar
 	public override void _Process(double delta)
 	{
 	}
+	
+	private void _on_healthbar_timer_timeout()
+	{
+		this.damageBar.Value = this.health;
+	}
+	
 }
+
+
+
