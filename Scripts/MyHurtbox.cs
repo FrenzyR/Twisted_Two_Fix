4516380aa18daa14d4d2f842ab2491247956058f
Frenzy_R <rubenverrod@gmail.com>
2024-02-21 12:40:45 +0100

    another fix

--- Scripts/MyHurtbox.cs
+++ Scripts/MyHurtbox.cs
@@ -4,28 +4,28 @@ namespace project_attempt.Scripts;
 
 public partial class MyHurtbox : Area2D
 {
-    public void Initialize()
-    {
-        CollisionLayer = 0;
-        CollisionMask = 2;
-    }
-    public override void _Ready()
-    {
-        Connect("area_entered", new Callable(this, "_on_area_entered"));
-    }
+	public void Initialize()
+	{
+		CollisionLayer = 0;
+		CollisionMask = 2;
+	}
+	public override void _Ready()
+	{
+		Connect("area_entered", new Callable(this, "_on_area_entered"));
+	}
 
-    private void _on_area_entered(MyHitbox hitbox)
-    {
-        GD.Print("hello");
-        if (hitbox == null)
-        {
-            return;
-        }
+	private void _on_area_entered(MyHitbox hitbox)
+	{
+		GD.Print("hello");
+		if (hitbox == null)
+		{
+			return;
+		}
 
-        if (Owner.HasMethod("Take_Damage"))
-        {
-            Owner.Call("Take_Damage", hitbox.Damage);
-        }
-    }
-    
+		if (Owner.HasMethod("Take_Damage"))
+		{
+			Owner.Call("Take_Damage", hitbox.Damage);
+		}
+	}
+	
 }
