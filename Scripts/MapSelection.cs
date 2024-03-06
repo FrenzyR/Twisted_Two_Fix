8ee33e23cfb674f5d7bcff6f46cb9ff53109f8d5
Frenzy_R <rubenverrod@gmail.com>
2024-03-06 12:58:35 +0100

    added more ways of acting to the map selection

--- Scripts/MapSelection.cs
+++ Scripts/MapSelection.cs
@@ -5,28 +5,46 @@ public partial class MapSelection : Node2D
 {
 
 	[Export] protected internal bool selectedMap = false;
+	PackedScene sceneLoaderScene = (PackedScene)GD.Load("Scenes/character_select.tscn");
+	Timer mapChangeTimer;
+	PackedScene packedScene = new PackedScene();
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		PackedScene sceneLoaderScene = (PackedScene)GD.Load("Scenes/character_select.tscn");
+		var mapSelect = GetNode<Sprite2D>("SelectionBG");
+		var mapOne = GetNode<Sprite2D>("MapOne");
+		var mapTwo = GetNode<Sprite2D>("MapTwo");
+		mapChangeTimer =  GetNode<Timer>("Timer");
+		
 		Node sceneInstance = sceneLoaderScene.Instantiate();
 		
-		PackedScene packedScene = new PackedScene();
+		
 		packedScene.Pack(sceneInstance);
 		
 		Node parentNode = GetParent();
 		if (Input.GetActionStrength("firstMap") != 0)
 		{
+			mapSelect.Visible = false;
+			mapOne.Visible = true;
 			GD.Print("Entered");
 			GetTree().ChangeSceneToPacked(packedScene);
 			GD.Print("Exited");
 		}
 		else if(Input.GetActionStrength("secondMap") != 0){
+			mapSelect.Visible = false;
+			mapTwo.Visible = true;
 			GD.Print("Entered");
 			selectedMap = true;
-			GetTree().ChangeSceneToPacked(packedScene);
+			mapChangeTimer.Start();
 			GD.Print("Exited");
 		}
 	}
+	private void _on_timer_timeout()
+	{
+			GetTree().ChangeSceneToPacked(packedScene);
+	}
+
 }
+
+
